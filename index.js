17f7a626d3caaf66fa04556234ef1a4c21a36ba6
jperkelens <jperkelens@gmail.com>
2015-06-09 10:28:45 -0400

    Adds Parent

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Elasticsearch = require('elasticsearch'),
   Search = require('./lib/search').Search,
   Update = require('./lib/search').Update,
-  Bulk = require('./lib/search').Bulk
+  Bulk = require('./lib/search').Bulk,
+  Index = require('./lib/search').Index
 
 module.exports = function(config) {
   var es = new Elasticsearch.Client(config)
@@ -9,6 +10,7 @@ module.exports = function(config) {
   return {
     search: new Search(es),
     update: new Update(es),
-    bulk: new Bulk(es)
+    bulk: new Bulk(es),
+    index: new Index(es)
   }
 }
